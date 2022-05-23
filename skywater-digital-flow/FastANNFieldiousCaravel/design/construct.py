7d577b50f3ac8ba13129ebcaaac8823889760d64
Chris <christophercalloway216@gmail.com>
2022-05-22 18:52:34 -0700

    added updates

--- skywater-digital-flow/FastANNFieldiousCaravel/design/construct.py
+++ skywater-digital-flow/FastANNFieldiousCaravel/design/construct.py
@@ -2,10 +2,10 @@
 #=========================================================================
 # construct.py
 #=========================================================================
-# Demo with 16-bit GcdUnit
+# Demo with a simple SRAM macro
 #
 # Author      : Priyanka Raina
-# Date        : December 4, 2020
+# Date        : February 15, 2021
 # Modified by : Allen Pan
 # Date        : March 15, 2022
 
@@ -25,16 +25,17 @@ def construct():
   adk_name = 'skywater-130nm-adk'
   adk_view = 'view-standard'
 
+  #TODO: Add more realistic TB
   parameters = {
     'construct_path' : __file__,
     'design_name'    : 'user_proj_example',
-    'clock_period'   : 10.0,
+    'clock_period'   : 6.6666666667,
     'adk'            : adk_name,
     'adk_view'       : adk_view,
     'topographical'  : True,
-    'testbench_name' : 'GcdUnitWrapperTb',
-    'saif_instance'  : 'GcdUnitWrapperTb/GcdUnitWrapper_inst',
-    'dut_name'       : 'GcdUnitWrapper_inst'
+    'testbench_name' : 'top_tb', 
+    'saif_instance'  : 'top_tb/dut',
+    'dut_name'       : 'dut'
   }
 
   #-----------------------------------------------------------------------
@@ -55,8 +56,11 @@ def construct():
   rtl             = Step( this_dir + '/rtl'                             )
   constraints     = Step( this_dir + '/constraints'                     )
   testbench       = Step( this_dir + '/testbench'                       )
-  custom_init     = Step( this_dir + '/custom-init'                     ) 
-  custom_flowstep = Step( this_dir + '/custom-flowstep'                 ) 
+  sram            = Step( this_dir + '/sram'                            )
+  pin_placement   = Step( this_dir + '/pin-placement'                   )
+  floorplan       = Step( this_dir + '/floorplan'                       )
+  syn_compile     = Step( this_dir + '/synopsys-dc-compile'             )
+
   # Power node is custom because power and gnd pins are named differently in
   # the standard cells compared to the default node, and the layer numbering is
   # different because of li layer, the default assumes metal 1 is the lowest
@@ -72,15 +76,16 @@ def construct():
 
   pt_timing       = Step( this_dir + '/synopsys-pt-timing-signoff'      )
 
-
   magic_drc       = Step( this_dir + '/open-magic-drc'                  )
   magic_def2spice = Step( this_dir + '/open-magic-def2spice'            )
   magic_gds2spice = Step( this_dir + '/open-magic-gds2spice'            )
-  netgen_lvs_gds  = Step( this_dir + '/netgen-lvs-gds'                  )
-  netgen_lvs_gds_device = Step( this_dir + '/netgen-lvs-gds-device'     )
-  netgen_lvs_def  = Step( this_dir + '/netgen-lvs-def'                  )
+  magic_gds2spice_nobbox = Step( this_dir + '/open-magic-gds2spice-nobbox'            )
+  netgen_lvs_def  = Step( this_dir + '/netgen-lvs-def'       )
   magic_antenna   = Step( this_dir + '/open-magic-antenna'              )
+  netgen_lvs_gds  = Step( this_dir + '/netgen-lvs-gds'                 )
+
   calibre_lvs     = Step( this_dir + '/mentor-calibre-comparison'       )
+  calibre_lvs_nobbox     = Step( this_dir + '/mentor-calibre-comparison-nobbox'       )
 
   # Need to use clone if you want to instantiate the same node more than once
   # in your graph but configure it differently, for example, RTL simulation and
@@ -94,7 +99,7 @@ def construct():
   # gl_sim          = icarus_sim.clone()
   rtl_sim.set_name( 'rtl-sim' )
   gl_sim.set_name(  'gl-sim'  )
-  
+
   pt_power       = Step( this_dir + '/synopsys-pt-power')
   pt_power_rtl   = pt_power.clone()
   pt_power_gl    = pt_power.clone()
@@ -104,8 +109,8 @@ def construct():
   # Default steps
 
   info            = Step( 'info',                          default=True )
-  dc              = Step( 'synopsys-dc-synthesis',         default=True )
-  
+  dc              = Step( 'synopsys-dc-synthesis',         default=True )  
+
   iflow           = Step( 'cadence-innovus-flowsetup',     default=True )
   init            = Step( 'cadence-innovus-init',          default=True )
   place           = Step( 'cadence-innovus-place',         default=True )
@@ -120,25 +125,26 @@ def construct():
   gen_saif_gl     = gen_saif.clone()
   gen_saif_rtl.set_name( 'gen-saif-rtl' )
   gen_saif_gl.set_name( 'gen-saif-gl' )
+
   
-  # Add extra input edges to innovus steps that need custom tweaks
-  init.extend_inputs ( custom_init.all_outputs()  )
-  iflow.extend_inputs ( custom_flowstep.all_outputs()  )
-  
+
   #-----------------------------------------------------------------------
   # Graph -- Add nodes
   #-----------------------------------------------------------------------
 
   g.add_step( info            )
+  g.add_step( sram            )
   g.add_step( rtl             )
   g.add_step( testbench       )
   g.add_step( rtl_sim         )
+  g.add_step( gl_sim          )
   g.add_step( constraints     )
+  g.add_step( syn_compile     )
   g.add_step( dc              )
   g.add_step( iflow           )
+  g.add_step( pin_placement   )
+  g.add_step( floorplan       )
   g.add_step( init            )
-  g.add_step( custom_init     )
-  g.add_step( custom_flowstep )
   g.add_step( power           )
   g.add_step( place           )
   g.add_step( cts             )
@@ -149,18 +155,18 @@ def construct():
   g.add_step( gdsmerge        )
   g.add_step( pt_timing       )
   g.add_step( gen_saif_rtl    )
-  g.add_step( pt_power_rtl    )
-  g.add_step( gl_sim          )
   g.add_step( gen_saif_gl     )
+  g.add_step( pt_power_rtl    )
   g.add_step( pt_power_gl     )
   g.add_step( magic_drc       )
   g.add_step( magic_antenna   )
   g.add_step( magic_def2spice )
   g.add_step( netgen_lvs_def  )
   g.add_step( magic_gds2spice )
+  g.add_step( magic_gds2spice_nobbox )
   g.add_step( netgen_lvs_gds  )
-  g.add_step( netgen_lvs_gds_device  )
   g.add_step( calibre_lvs     )
+  g.add_step( calibre_lvs_nobbox     )
 
   #-----------------------------------------------------------------------
   # Graph -- Add edges
@@ -168,13 +174,42 @@ def construct():
   
   # Dynamically add edges
 
-  rtl_sim.extend_inputs(['test_vectors.txt'])
-  gl_sim.extend_inputs(['test_vectors.txt'])
+  rtl_sim.extend_inputs(['sky130_sram_1kbyte_1rw1r_32x256_8.v'])
+  rtl_sim.extend_inputs(['expected_index.txt'])
+  rtl_sim.extend_inputs(['internalNodes.txt'])
+  rtl_sim.extend_inputs(['leafNodes.txt'])
+  rtl_sim.extend_inputs(['patches.txt'])
+  
+  gl_sim.extend_inputs(['sky130_sram_1kbyte_1rw1r_32x256_8.v'])
+  gl_sim.extend_inputs(['expected_index.txt'])
+  gl_sim.extend_inputs(['internalNodes.txt'])
+  gl_sim.extend_inputs(['leafNodes.txt'])
+  gl_sim.extend_inputs(['patches.txt'])
+  
+  
+  
+  
+  dc.extend_inputs(['sky130_sram_1kbyte_1rw1r_32x256_8_TT_1p8V_25C.db'])
+  dc.extend_inputs(['sky130_sram_1kbyte_1rw1r_32x256_8.lef'])
+  pt_timing.extend_inputs(['sky130_sram_1kbyte_1rw1r_32x256_8_TT_1p8V_25C.db'])
+  pt_power_rtl.extend_inputs(['sky130_sram_1kbyte_1rw1r_32x256_8_TT_1p8V_25C.db'])
+  pt_power_gl.extend_inputs(['sky130_sram_1kbyte_1rw1r_32x256_8_TT_1p8V_25C.db'])
+  gdsmerge.extend_inputs(['sky130_sram_1kbyte_1rw1r_32x256_8.gds'])
+  # netgen_lvs_def.extend_inputs(['sky130_sram_1kbyte_1rw1r_32x256_8.sp'])
+  # netgen_lvs_gds.extend_inputs(['sky130_sram_1kbyte_1rw1r_32x256_8.sp'])
+  calibre_lvs.extend_inputs(['sky130_sram_1kbyte_1rw1r_32x256_8.sp'])
+  calibre_lvs_nobbox.extend_inputs(['sky130_sram_1kbyte_1rw1r_32x256_8.sp'])
+  magic_drc.extend_inputs(['sky130_sram_1kbyte_1rw1r_32x256_8.lef'])
+  
+  for step in [iflow, init, power, place, cts, postcts_hold, route, postroute, signoff]:
+    step.extend_inputs(['sky130_sram_1kbyte_1rw1r_32x256_8_TT_1p8V_25C.lib', 'sky130_sram_1kbyte_1rw1r_32x256_8.lef'])
 
+  init.extend_inputs(['floorplan.tcl', 'pin-assignments.tcl'])
+  dc.extend_inputs(['compile.tcl'])
   # Connect by name
 
-  g.connect_by_name( adk,             dc              )
   g.connect_by_name( adk,             testbench       )
+  g.connect_by_name( adk,             dc              )
   g.connect_by_name( adk,             iflow           )
   g.connect_by_name( adk,             init            )
   g.connect_by_name( adk,             power           )
@@ -189,10 +224,11 @@ def construct():
   g.connect_by_name( adk,             magic_antenna   )
   g.connect_by_name( adk,             magic_def2spice )
   g.connect_by_name( adk,             magic_gds2spice )
+  g.connect_by_name( adk,             magic_gds2spice_nobbox )
   g.connect_by_name( adk,             netgen_lvs_def  )
   g.connect_by_name( adk,             netgen_lvs_gds  )
-  g.connect_by_name( adk,             netgen_lvs_gds_device  )
   g.connect_by_name( adk,             calibre_lvs     )
+  g.connect_by_name( adk,             calibre_lvs_nobbox     )
   g.connect_by_name( adk,             pt_timing       )
   g.connect_by_name( adk,             pt_power_rtl    )
   g.connect_by_name( adk,             pt_power_gl     )
@@ -201,8 +237,33 @@ def construct():
   g.connect_by_name( rtl,             rtl_sim         ) # design.v
   g.connect_by_name( testbench,       rtl_sim         ) # testbench.sv
   g.connect_by_name( rtl_sim,         gen_saif_rtl    ) # run.vcd
-  
+
+  g.connect_by_name( sram,            rtl_sim         )
+  g.connect_by_name( sram,            gl_sim          )
+  g.connect_by_name( sram,            dc              )
+  g.connect_by_name( sram,            iflow           )
+  g.connect_by_name( sram,            init            )
+  g.connect_by_name( sram,            power           )
+  g.connect_by_name( sram,            place           )
+  g.connect_by_name( sram,            cts             )
+  g.connect_by_name( sram,            postcts_hold    )
+  g.connect_by_name( sram,            route           )
+  g.connect_by_name( sram,            postroute       )
+  g.connect_by_name( sram,            signoff         )
+  g.connect_by_name( sram,            gdsmerge        )
+  g.connect_by_name( sram,            pt_timing       )
+  g.connect_by_name( sram,            pt_power_rtl    )
+  g.connect_by_name( sram,            pt_power_gl     )
+  g.connect_by_name( sram,            magic_def2spice )
+  g.connect_by_name( sram,            magic_gds2spice )
+  g.connect_by_name( sram,            netgen_lvs_def  )
+  g.connect_by_name( sram,            netgen_lvs_gds  )
+  g.connect_by_name( sram,            calibre_lvs     )
+  g.connect_by_name( sram,            calibre_lvs_nobbox     )
+  g.connect_by_name( sram,            magic_drc       )
+
   g.connect_by_name( rtl,             dc              )
+  g.connect_by_name( syn_compile,     dc              )
   g.connect_by_name( constraints,     dc              )
   g.connect_by_name( gen_saif_rtl,    dc              ) # run.saif
   
@@ -222,12 +283,8 @@ def construct():
   g.connect_by_name( iflow,           signoff         )
   
   # Core place and route flow
-  g.connect_by_name( custom_init,     init            )
-  order = init.get_param( 'order' )
-  order.append( 'dont-use-cells.tcl' )
-
-  g.connect_by_name( custom_flowstep, iflow           )
-  
+  g.connect_by_name( floorplan,       init            )
+  g.connect_by_name( pin_placement,   init            )
   g.connect_by_name( init,            power           )
   g.connect_by_name( power,           place           )
   g.connect_by_name( place,           cts             )
@@ -247,15 +304,18 @@ def construct():
   g.connect_by_name( magic_def2spice, netgen_lvs_def  )
 
   # LVS using GDS
-  g.connect_by_name( gdsmerge,        magic_gds2spice )
   g.connect_by_name( signoff,         netgen_lvs_gds  )
-  g.connect_by_name( signoff,         netgen_lvs_gds_device  )
-  g.connect_by_name( magic_gds2spice, netgen_lvs_gds  )
-  g.connect_by_name( magic_gds2spice, netgen_lvs_gds_device  )
+  g.connect_by_name( magic_gds2spice_nobbox, netgen_lvs_gds  )
 
-  # LVS comparision using Calibre
+  # LVS comparision using Calibre with standard cells blackboxed
+  g.connect_by_name( gdsmerge,        magic_gds2spice )
   g.connect_by_name( signoff,         calibre_lvs     )
   g.connect_by_name( magic_gds2spice, calibre_lvs     )
+
+  # LVS comparision using Calibre without standard cells blackboxed
+  g.connect_by_name( gdsmerge,        magic_gds2spice_nobbox )
+  g.connect_by_name( signoff,         calibre_lvs_nobbox     )
+  g.connect_by_name( magic_gds2spice_nobbox, calibre_lvs_nobbox     )
 
   # Timing signoff
   g.connect( signoff.o('design.spef.gz'),   pt_timing.i('design.spef.gz' ) )
@@ -263,13 +323,13 @@ def construct():
   g.connect( dc.o(     'design.sdc'    ),   pt_timing.i('design.pt.sdc'  ) )
 
   # Gate level simulation
-  g.connect( signoff.o(   'design.vcs.pg.v'  ), gl_sim.i( 'design.vcs.v'     ) )
-  g.connect( pt_timing.o( 'design.sdf'       ), gl_sim.i( 'design.sdf'       ) )
-  g.connect( testbench.o( 'design.args.gls'  ), gl_sim.i( 'design.args'      ) )
-  g.connect( testbench.o( 'test_vectors.txt' ), gl_sim.i( 'test_vectors.txt' ) )
-  g.connect( testbench.o( 'testbench.sv'     ), gl_sim.i( 'testbench.sv'     ) )
+  g.connect( signoff.o(   'design.vcs.pg.v'  ), gl_sim.i(      'design.vcs.v'     ) )
+  g.connect( pt_timing.o( 'design.sdf'       ), gl_sim.i(      'design.sdf'   ) )
+  g.connect( testbench.o( 'testbench.sv'     ), gl_sim.i(      'testbench.sv' ) )
+  g.connect( testbench.o( 'design.args.gls'  ), gl_sim.i(      'design.args'  ) )
   g.connect_by_name( gl_sim,                    gen_saif_gl    ) # run.vcd
 
+  # and power signoff
   g.connect( signoff.o('design.spef.gz'),   pt_power_rtl.i('design.spef.gz' ) )
   g.connect( signoff.o('design.vcs.v'  ),   pt_power_rtl.i('design.vcs.v'   ) )
   g.connect( dc.o(     'design.sdc'    ),   pt_power_rtl.i('design.pt.sdc'  ) )
