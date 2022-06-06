3c29834138b77ffbbd82ffa450e95d9e3da108f6
Chris-Not-Mikey <54165966+Chris-Not-Mikey@users.noreply.github.com>
2022-06-06 14:48:54 -0700

    Update SramUnit.v

--- skywater-digital-flow/FastANNFieldiousCaravel/design/rtl/SramUnit.v
+++ skywater-digital-flow/FastANNFieldiousCaravel/design/rtl/SramUnit.v
@@ -2114,8 +2114,8 @@ module MainFSM (
 	parameter DATA_WIDTH = 11;
 	parameter LEAF_SIZE = 8;
 	parameter PATCH_SIZE = 5;
-	parameter ROW_SIZE = 28;
-	parameter COL_SIZE = 19;
+	parameter ROW_SIZE = 32;
+	parameter COL_SIZE = 18;
 	parameter NUM_QUERYS = ROW_SIZE * COL_SIZE;
 	parameter K = 4;
 	parameter NUM_LEAVES = 64;
@@ -3718,8 +3718,8 @@ module top (
 	parameter IDX_WIDTH = 9;
 	parameter LEAF_SIZE = 8;
 	parameter PATCH_SIZE = 5;
-	parameter ROW_SIZE = 28;
-	parameter COL_SIZE = 19;
+	parameter ROW_SIZE = 32;
+	parameter COL_SIZE = 18;
 	parameter NUM_QUERYS = ROW_SIZE * COL_SIZE;
 	parameter K = 4;
 	parameter BEST_ARRAY_K = 1;
