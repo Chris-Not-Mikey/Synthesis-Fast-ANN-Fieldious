b5f6c1cc648d236b60a0ca121338bbcadfd75aa9
Jake Ke <jackieke724@hotmail.com>
2022-06-10 19:59:09 -0700

    merged upstream changes

--- skywater-digital-flow/WrapperSignoff/design/mentor-calibre-lvs/device_name_parser.sh
+++ skywater-digital-flow/WrapperSignoff/design/mentor-calibre-lvs/device_name_parser.sh
@@ -5,5 +5,8 @@ sed -i -r 's/([wl]=)([1-9]+)0*u/\10.\2/g' $1
 sed -i -r 's/([wl]=)(\S+?)e\+06u/\1\2/g' $1
 sed -i -r 's/^X([[:digit:]])/M\1/g' $1
 sed -i -r 's/sky130_fd_pr__pfet_01v8/pmos/g' $1
-sed -i -r 's/sky130_fd_pr__nfet_01v8/nmos/g' $1
+sed -i -r 's/sky130_fd_pr__pfet_01v8/pmos/g' $1
+sed -i -r 's/R0\sVGND\sLO\ssky130_fd_pr__res_generic_po\sw=0.48\sl=0.45/\.connect LO VGND/g' $1
+sed -i -r 's/R1\sHI\sVPWR\ssky130_fd_pr__res_generic_po\sw=0.48\sl=0.45/\.connect HI VPWR/g' $1
+
 sed -i -r 's/\//_/g' $1
